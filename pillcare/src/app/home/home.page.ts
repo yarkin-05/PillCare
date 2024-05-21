fe9dd694dd586493d545f670685e0fe6fb0076b2
mar <16024@portalcedi.edu.mx marm0r>
2024-05-21 09:51:33 -0600

    html, css, ts

--- pillcare/src/app/home/home.page.ts
+++ pillcare/src/app/home/home.page.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nombre = "ibuprofeno";
+  dosis = 300;
+  dia = 11;
+  mes = "mayo"
 
   constructor() {}
 

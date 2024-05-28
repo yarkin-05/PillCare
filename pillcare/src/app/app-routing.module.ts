2c0fac55dce0a6efcbdc5c750670a5c94eef9113
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-05-28 09:45:44 -0600

    new branch to work on other things

--- pillcare/src/app/app-routing.module.ts
+++ pillcare/src/app/app-routing.module.ts
@@ -11,18 +11,6 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'sign-up',
-    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
-  },
-  {
-    path: 'password-reset',
-    loadChildren: () => import('./password-reset/password-reset.module').then( m => m.PasswordResetPageModule)
-  },
 ];
 
 @NgModule({

8800057467948b1cdb561418b6c1c0ff0a46012f
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-06-04 07:59:35 -0600

    added first page with functional links

--- pillcare/src/app/first/first.page.ts
+++ pillcare/src/app/first/first.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { environment } from 'src/environments/environment';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-first',
@@ -7,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController, private navController: NavController) { }
 
   ngOnInit() {
   }

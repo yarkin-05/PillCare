aa92a31f2223a169d7cdf9a3d1408b94e4adc7f0
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-06-04 23:22:31 -0600

    added the calendar plugin

--- pillcare/src/app/home/home.page.ts
+++ pillcare/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Calendar } from '@awesome-cordova-plugins/calendar/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  nombre = "ibuprofeno";
-  dosis = 300;
-  dia = 11;
-  mes = "mayo"
 
-  constructor(private router: Router) {}
+   // Create a new Date object
+   date = new Date();
 
+   // Get the current day and month
+   dia = this.date.getDate();
+   mes = this.date.toLocaleString('default', { month: 'long' });
+ 
+    // Get the current pill
+   pillName: string = "";
+  constructor(private router: Router, private calendar: Calendar) {}
 
+  ngOnInit(){
+    let startDate = new Date();
+    startDate.setHours(0, 0, 0, 0);
+
+    let endDate = new Date();
+    endDate.setHours(23, 59, 59, 999);
+
+    this.calendar.findEvent('', '', '', startDate, endDate).then((data) => {
+      console.log(data);
+      if(data.length > 0){
+        this.pillName = data[0].title;
+      }
+    
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+  
   navigateToPastilla(){
     this.router.navigateByUrl('/pastilla')
   }
